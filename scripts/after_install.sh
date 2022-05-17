bd89da7a47d861e40730458685286fb6776216f0
Chinmay Anaokar <chinmay@brainfloss.edu.in>
2022-05-17 20:56:05 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,15 +2,3 @@
 
 # navigate to app folder
 cd /app
-
-# install dependencies
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-/home/ec2-user/.nvm/versions/node/v14.19.2/bin/npm i
-/home/ec2-user/.nvm/versions/node/v14.19.2/bin/npx frontity build
-
-# install pm2 process manager
-/home/ec2-user/.nvm/versions/node/v14.19.2/bin/npm install pm2 -g
